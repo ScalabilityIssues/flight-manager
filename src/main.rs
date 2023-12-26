7e48f03e7e9acc27a3c00ae8dd023d0a11eea0ef
JonM0 <61517667+JonM0@users.noreply.github.com>
2023-12-26 21:47:43 +0100

    Enable tracing and web-grpc

--- src/main.rs
+++ src/main.rs
@@ -23,33 +23,42 @@ struct Opt {
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt().init();
+    tracing_subscriber::fmt().with_max_level(tracing::Level::DEBUG).init();
 
     let opt = Opt::parse();
 
     let db_pool = PgPool::connect(&opt.db).await?;
 
+    // run migrations
     tracing::info!("running migrations");
     sqlx::migrate!().run(&db_pool).await?;
 
+    // bind server socket
     let addr = SocketAddr::new(opt.ip, opt.port);
     let listener = TcpListener::bind(addr).await?;
-
     tracing::info!("starting server on {}", addr);
 
     let reflection = tonic_reflection::server::Builder::configure()
         .register_encoded_file_descriptor_set(planes::proto::FILE_DESCRIPTOR_SET)
         .build()?;
 
+    let cors = tower_http::cors::CorsLayer::new()
+        .allow_headers(tower_http::cors::Any)
+        .allow_methods([http::Method::POST])
+        .allow_origin(["http://localhost:3000".parse()?]);
+
     Server::builder()
+        // configure the server
+        .accept_http1(true)
         .timeout(std::time::Duration::from_secs(10))
-        // .layer(tower_http::trace::TraceLayer::new_for_grpc())  // <- broken
-        .layer(tonic::service::interceptor(|req| {
-            tracing::info!("received request {:?}", req);
-            Ok(req)
-        }))
+        .layer(cors)
+        .layer(tower_http::trace::TraceLayer::new_for_grpc())
+        .layer(tonic_web::GrpcWebLayer::new())
+        // cnable grpc reflection
         .add_service(reflection)
+        // add services
         .add_service(PlanesServer::new(PlanesApp::new(db_pool)))
+        // serve
         .serve_with_incoming(TcpListenerStream::new(listener))
         .await?;
 
